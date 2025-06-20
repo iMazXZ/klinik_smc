5169eef67c5918e027f2d9eb3819e3f202caff1e
iMazXZ <akungamemaulana@gmail.com>
2025-06-20 14:52:25 +0700

    main

--- app/Filament/Resources/ReservationResource/Pages/ViewReservation.php
+++ app/Filament/Resources/ReservationResource/Pages/ViewReservation.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\ReservationResource\Pages;
 
 use App\Filament\Resources\ReservationResource;
-use App\Models\Reservation; // <-- Import model Reservation
+use App\Models\Reservation;
 use Filament\Resources\Pages\Page;
 
 class ViewReservation extends Page
@@ -12,10 +12,8 @@ class ViewReservation extends Page
 
     protected static string $view = 'filament.resources.reservation-resource.pages.view-reservation';
 
-    // Properti untuk menyimpan data reservasi yang akan ditampilkan
     public Reservation $record;
 
-    // Method `mount` akan mengambil data berdasarkan ID dari URL
     public function mount(int | string $record): void
     {
         $this->record = Reservation::findOrFail($record);
